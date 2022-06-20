0052f85517e7b5f941bece71f44343ebb53c6ba4
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-06-21 01:35:47 +0530

    fix: breeds re-render issue

--- src/components/Breeds.js
+++ src/components/Breeds.js
@@ -1,16 +1,16 @@
 import Breed from "./Breed"
 
 function Breeds({ list, error, loading }) {
-  if (loading) return "Loading..."
-
-  if (error) return `Error fetching breeds ${error?.message}`
-
   return (
-    <div className="Breeds">
-      {list.map((item) => (
-        <Breed key={item.breed} value={item} />
-      ))}
-    </div>
+    <>
+      <div className="Breeds">
+        {list.map((item, index) => (
+          <Breed key={index} value={item} />
+        ))}
+      </div>
+      {loading && "Loading..."}
+      {error && `Error fetching breeds ${error?.message}`}
+    </>
   )
 }
 
