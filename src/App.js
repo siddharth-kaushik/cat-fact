fcef01412651ec06f5bb9c33ccd2c2c456480aa3
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-06-21 02:55:41 +0530

    refactor: cleanup app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import { useCallback, useEffect } from "react"
-
-import { useBreeds, useFact, useFilteredBreeds } from "./hooks"
+import { useBreeds, useFact, useFilteredBreeds, useWindowScroll } from "./hooks"
 import { Breeds, Fact, SearchBar } from "./components"
-import { useCatState, useCatDispatch } from "./context"
 
 import "./App.css"
 
@@ -10,39 +7,14 @@ function App() {
   useFact()
   useBreeds()
   useFilteredBreeds()
-
-  const dispatch = useCatDispatch()
-  const { breeds, breedsLoading, hasMore, filtered, page } = useCatState()
-  const stat = `${filtered.length} / ${breeds.length}`
-
-  const onScroll = useCallback(
-    (e) => {
-      const buffer = 300
-      const { scrollHeight, scrollTop } = e.target.documentElement
-      const windowHeight = window.innerHeight
-
-      const reachingEnd = scrollTop + windowHeight + buffer > scrollHeight
-
-      if (reachingEnd && !breedsLoading && hasMore) {
-        dispatch({ type: "page", payload: page + 1 })
-      }
-    },
-    [hasMore, breedsLoading]
-  )
-
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll)
-    return () => window.removeEventListener("scroll", onScroll)
-  }, [onScroll])
+  useWindowScroll()
 
   return (
     <div className="App">
       <h1>The Cat Fact App</h1>
       <Fact />
       <SearchBar />
-      <h3>{stat}</h3>
       <Breeds />
-      {!hasMore && <div>No more breeds...</div>}
     </div>
   )
 }
