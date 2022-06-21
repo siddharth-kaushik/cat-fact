456d969bcde487d2f2feadd3317d2dc088a0d65f
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-06-21 10:03:33 +0530

    feat: add overline and caption component

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,5 +1,7 @@
 import GlobalStyles from "./GlobalStyles"
 import Heading from "./Heading"
 import Text from "./Text"
+import Caption from "./Caption"
+import Overline from "./Overline"
 
-export { GlobalStyles, Heading, Text }
+export { GlobalStyles, Heading, Text, Caption, Overline }
