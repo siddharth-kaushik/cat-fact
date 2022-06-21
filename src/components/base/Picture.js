27cda6eb7d07c18408dd013be8ec22d2596b4cb0
Siddharth Kaushik <sidharth.basu@icloud.com>
2022-06-21 17:51:42 +0530

    update: picture to incorporate for poistioning

--- src/components/base/Picture.js
+++ src/components/base/Picture.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 function Picture({ avif, webp, fallback, alt, ...rest }) {
+  const width = rest.width || "100%"
+  const height = rest.height || "100%"
+
   return (
-    <picture>
+    <picture {...rest}>
       {avif && <source srcSet={avif} type="image/avif" />}
       {webp && <source srcSet={webp} type="image/webp" />}
-      <img alt={alt} src={fallback} {...rest} />
+      <img alt={alt} src={fallback} width={width} height={height} />
     </picture>
   )
 }
