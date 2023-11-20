22f97cb4ff4b85aa9a6010b7eb738e932c3ebfee
Siddharth Kaushik <sidharth.basu@icloud.com>
2023-11-20 01:56:59 -0800

    use createRoot to render

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import { CatProvider } from "./context"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = document.getElementById("root")
-ReactDOM.render(
+import { CatProvider } from './context'
+import App from './App'
+
+import './index.css'
+
+const container = document.getElementById('_app')
+const root = createRoot(container)
+root.render(
   <React.StrictMode>
     <CatProvider>
       <App />
     </CatProvider>
   </React.StrictMode>,
-  root
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
